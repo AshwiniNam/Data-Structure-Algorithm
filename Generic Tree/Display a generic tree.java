78293a1d95bb85e65bed72d4ee26fe256d8f14ec
Ashwini Namboothiri <60285499+AshwiniNam@users.noreply.github.com>
2022-07-28 10:15:44 +0530

    Update Display a generic tree.java

--- Generic Tree/Display a generic tree.java
+++ Generic Tree/Display a generic tree.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.io.*;
+public class Main{
 //create a Node class
 public class Node{
     int data;
@@ -18,7 +20,7 @@ public static Node construct(int[] arr){
         {
             Node t = new Node();
             t.data = arr[i];
-            if(st.size>0)
+            if(st.size()>0)
             {
                 st.peek().children.add(t); //add new node t to the arraylist of parent(st.peek())
             }
@@ -32,19 +34,31 @@ public static Node construct(int[] arr){
     return root;
 }
 
-public static void display(int[] arr){
-    
+public static void display(Node root){
+    System.out.print(root+"->");
+    for(int i=0;i<root.children.size();i++)
+    {
+        System.out.print(root.children.get(i).data+",");
+    }
+    System.out.println(".");
+    for(int i=0;i<root.children.size();i++)
+    {
+        Node child = root.children.get(i);
+        display(child);
+    }
 }
 
-public static public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
-    int size = scn.nextInt();
-    int[] arr = new int[size];
-    for(int i=0;i<size;i++)
-    {
-        arr[i] = scn.nextInt();
+public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
     }
+
     Node root = construct(arr);
     display(root);
+}
 }
 //here input arr whould be [1,2,5,-1,6,-1,-1,3,7,-1,-1,-1]

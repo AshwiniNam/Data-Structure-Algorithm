238a6c61ca9883b5c3c35169365f18e6026e551d
Ashwini Namboothiri <60285499+AshwiniNam@users.noreply.github.com>
2022-08-08 10:27:54 +0530

    Create Diameter Of Generic Tree.java

---
+++ Generic Tree/Diameter Of Generic Tree.java
@@ -0,0 +1,79 @@
+#my logic-works fine
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void display(Node node) {
+        String str = node.data + " -> ";
+        for (Node child : node.children) {
+            str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+
+        for (Node child : node.children) {
+            display(child);
+        }
+    }
+
+    public static Node construct(int[] arr) {
+        Node root = null;
+
+        Stack<Node> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) {
+                st.pop();
+            } else {
+                Node t = new Node();
+                t.data = arr[i];
+
+                if (st.size() > 0) {
+                    st.peek().children.add(t);
+                } else {
+                    root = t;
+                }
+
+                st.push(t);
+            }
+        }
+
+        return root;
+    }
+
+    static int secmaxheight;
+
+    public static int diameter(Node node) {
+        int dm = -1;
+        for (Node child : node.children) {
+            int dmn = diameter(child);
+            if (dmn > dm) {
+                dm = dmn;
+            } else if (dmn == dm || dmn > secmaxheight) {
+                secmaxheight = dmn;
+            }
+        }
+        dm = dm + 1;
+        return dm;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(values[i]);
+        }
+
+        Node root = construct(arr);
+        int ans = diameter(root);
+        System.out.println(ans + 1 + secmaxheight);
+    }
+
+}

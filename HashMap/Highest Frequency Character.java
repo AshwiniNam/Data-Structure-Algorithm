4aaf781e5b4de3a64baffdd3e0c3f4ac40e89ae0
Ashwini Namboothiri <60285499+AshwiniNam@users.noreply.github.com>
2022-08-11 23:24:06 +0530

    Update Highest Frequency Character.java

--- HashMap/Highest Frequency Character.java
+++ HashMap/Highest Frequency Character.java
@@ -6,23 +6,26 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         String s = scn.next();
-        char ans = ' ';
-        int max = Integer.MIN_VALUE;
+        // char ans=' ';
+        // int max = Integer.MIN_VALUE;
         HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (map.containsKey(ch)) {
                 int val = map.get(ch);
-                if (val + 1 > max) {
-                    max = val + 1;
-                    ans = ch;
-                }
                 map.put(ch, val + 1);
             } else {
                 map.put(ch, 1);
             }
         }
-        System.out.println(ans);
+        // using keySet function
+        char max = s.charAt(0);
+        for (char key : map.keySet()) {
+            if (map.get(key) > map.get(max)) {
+                max = key;
+            }
+        }
+        System.out.println(max);
     }
 
 }

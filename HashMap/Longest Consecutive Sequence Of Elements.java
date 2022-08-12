0bcea5978eb5763de91923744b0609e9917df8d0
Ashwini Namboothiri <60285499+AshwiniNam@users.noreply.github.com>
2022-08-12 21:02:10 +0530

    Update Longest Consecutive Sequence Of Elements.java

--- HashMap/Longest Consecutive Sequence Of Elements.java
+++ HashMap/Longest Consecutive Sequence Of Elements.java
@@ -1,5 +1,3 @@
-//My approach
-
 import java.io.*;
 import java.util.*;
 
@@ -8,42 +6,47 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> norep = new ArrayList<>();
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        // step 1
         for (int i = 0; i < n; i++) {
             int num = scn.nextInt();
-            if (map.containsKey(num)) {
-                int val = map.get(num);
-                map.put(num, val + 1);
-            } else {
-                map.put(num, 1);
-                norep.add(num);
+            if (map.containsKey(num) == false) {
+                map.put(num, true);
+                arr.add(num);
             }
         }
 
-        ArrayList<Integer> maxarr = new ArrayList<>();
-        for (int i = 0; i < norep.size(); i++) {
-            ArrayList<Integer> arr = new ArrayList<>();
-            int val = norep.get(i);
-            while (map.containsKey(val)) {
-                arr.add(val);
-                val = val + 1;
-            }
+        // step 2
+        for (int key : map.keySet()) {
+            if (map.containsKey(key - 1))
+                map.put(key, false);
+        }
 
-            if (arr.size() > maxarr.size()) {
-                for (int j = 0; j < maxarr.size(); j++) {
-                    // System.out.print(arr.get(j)+" ");
-                    maxarr.set(j, arr.get(j));
+        // step 3
+        int startval = 0;
+        int length = 1;
+        for (int key : map.keySet()) {
+            if (map.containsKey(key)) {
+                int start = key;
+                int size = 0;
+                while (map.containsKey(start)) {
+                    size = size + 1;
+                    start += 1;
                 }
 
-                for (int j = maxarr.size(); j < arr.size(); j++) {
-
-                    maxarr.add(arr.get(j));
+                if (size > length) {
+                    startval = key;
+                    length = size;
                 }
             }
         }
-        for (int k = 0; k < maxarr.size(); k++) {
-            System.out.println(maxarr.get(k));
+
+        while (length > 0) {
+            System.out.println(startval);
+            startval += 1;
+            length--;
         }
     }
 
